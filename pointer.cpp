9e5d88663ff1b22345256326f4f873f00afd151b
SunnyAman11 <148849088+SunnyAman11@users.noreply.github.com>
2023-11-11 19:26:44 +0530

    Update pointer.cpp
    this is simple c++ code of pointers                                                             the output is                                                                                        6
    6

--- pointer.cpp
+++ pointer.cpp
@@ -3,13 +3,12 @@ using namespace std;
     #include<conio.h>
     int main(){
     int i=5;
+   cout<<i<<"\n";
     int*p=&i;
     int**q=&p; 
-    (*q)++;
+    
     *p++;
     cout<<i<<"\n";
-    cout<<i<<"\n";
-   
 
     return 0;
 }
